b80d98815c2ba249f83b1ee1c60baa9bda609b2f
yasmojam <yazzij97@gmail.com>
2020-03-15 15:46:55 +0000

    Volume settings screen

---
+++ Connect4prototype/app/src/main/java/com/example/connect4prototype/ImageAdapter.java
@@ -0,0 +1,125 @@
+package com.example.connect4prototype;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private int selectedPosition = -1;
+    private View selectedView = null;
+
+    int player1Counter = Color.RED;
+    int player2Counter = Color.YELLOW;
+
+    private int numCounters = 0;
+
+    private int maxCounters = 42; //6 x 7 board
+
+    private ArrayList<Integer> selectedPositions = new ArrayList<Integer>();
+
+
+
+    private ArrayList<ImageView> boardPieces = new ArrayList<>();
+
+    //Keep all images in array
+    public Integer[] mThumbIds = {
+            R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board,
+            R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board,
+            R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board,
+            R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board,
+            R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board,
+            R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board, R.drawable.board
+    };
+
+
+    //Constructor
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+        getBoardPieces().add(imageView);
+
+        if (getSelectedPositions().contains(position) && getNumCounters()%2 != 0) {
+            //boardPieces.get(getSelectedPositions().indexOf(position)).setBackgroundColor(player1Counter);
+            imageView.setBackgroundColor(player1Counter);
+        }
+        else if (getSelectedPositions().contains(position) && getNumCounters()%2 == 0){
+            //getBoardPieces().get(getSelectedPositions().indexOf(position)).setBackgroundColor(player2Counter);
+            imageView.setBackgroundColor(player2Counter);
+        }
+        else {
+            ;
+        }
+            return imageView;
+        }
+
+//
+
+
+//        if (position == selectedPosition){
+//            imageView.setBackgroundColor(Color.RED);
+//        }
+//        else{
+//            //imageView.setBackgroundColor(Color.TRANSPARENT);
+//            ;
+
+
+    public ArrayList<ImageView> getBoardPieces() {
+        return boardPieces;
+    }
+
+    public void addSelectedPositions(int selectedPosition){
+        selectedPositions.add(selectedPosition);
+    }
+
+    public ArrayList<Integer> getSelectedPositions() {
+        return selectedPositions;
+    }
+
+    public void setView(View view){
+        selectedView = view;
+    }
+    public void setSelectedPosition(int position) {
+        selectedPosition = position;
+    }
+
+    public int getNumCounters(){
+        return numCounters;
+    }
+
+    public void setNumCounters(int numCounters) {
+        this.numCounters = numCounters;
+    }
+
+    public int getMaxCounters() {
+        return maxCounters;
+    }
+}
---
+++ MediumFidelity3/app/src/main/java/com/example/mediumfidelity3/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mediumfidelity3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch darkMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        }
+        else {
+            setTheme(R.style.AppTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        darkMode = findViewById(R.id.darkModeSwitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            darkMode.setChecked(true);
+        }
+        darkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
+    }
+
+    public void restartApp() {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
