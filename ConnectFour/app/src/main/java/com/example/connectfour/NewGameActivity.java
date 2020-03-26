7b97f4889264fd00dc1fc1a1aa206e22aa9aadcb
holliemb <mhb19165@uni.strath.ac.uk>
2020-03-26 11:18:10 +0000

    Fixed sizing issue with 'Dark Mode Switch and Text'

--- ConnectFour/app/src/main/java/com/example/connectfour/NewGameActivity.java
+++ ConnectFour/app/src/main/java/com/example/connectfour/NewGameActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
@@ -112,6 +113,9 @@ public class NewGameActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 
 
 }
